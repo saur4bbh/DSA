49c0721aa9adc84fe238b3d4a23e64930f7dc97c
Saurabh <134841560+saur4bbh@users.noreply.github.com>
2025-08-31 16:02:07 +0200

    Time: 2 ms (57.11%), Space: 18.1 MB (39.33%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -1,47 +1,41 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        if len(nums1) > len(nums2):
+            nums1, nums2 = nums2, nums1
+
         m = len(nums1)
         n = len(nums2)
-        total = m + n
 
-        mid1 = (m + n - 1) // 2
-        mid2 = (m + n) // 2
+        half = (m + n + 1) // 2
 
-        ele1 = ele2 = 0
+        l, r = 0, m # no. of ele from smaller array in final array
 
-        cnt = 0
-        i = j = 0
-        while i < m and j < n:
-            if nums1[i] <= nums2[j]:
-                ele1, ele2 = nums1[i],ele1
-                i += 1
+        left = right = 0 # both mid of final array
+
+        while l <= r:
+            mid = (l + r) // 2
+            mid2 = half - mid
+
+            r1 = r2 = float('inf')
+            l1 = l2 = float('-inf')
+
+            if mid < m: r1 = nums1[mid]
+            if mid-1 >= 0: l1 = nums1[mid-1]
+            if mid2 < n: r2 = nums2[mid2]
+            if mid2-1 >= 0: l2 = nums2[mid2-1]
+
+            if l1 <= r2 and l2 <= r1:
+                left = max(l1, l2)
+                right = min(r1, r2)
+                break
+
+            elif l2 > r1:
+                l = mid + 1
+
             else:
-                ele1, ele2 = nums2[j],ele1
-                j += 1
-
-            if cnt == mid2:
-                if (m+n) % 2 == 0:
-                    return (ele1 + ele2) / 2
-                else:
-                    return ele1
-            cnt += 1
-
-        while i<m:
-            ele1, ele2 = nums1[i],ele1
-            i += 1
-            if cnt == mid2:
-                if (m+n) % 2 == 0:
-                    return (ele1 + ele2) / 2
-                else:
-                    return ele1
-            cnt += 1
-
-        while j<n:
-            ele1, ele2 = nums2[j],ele1
-            j += 1
-            if cnt == mid2:
-                if (m+n) % 2 == 0:
-                    return (ele1 + ele2) / 2
-                else:
-                    return ele1
-            cnt += 1
+                r = mid - 1
+        
+        if (m + n) % 2 == 0:
+            return (left + right) / 2
+        else:
+            return left
