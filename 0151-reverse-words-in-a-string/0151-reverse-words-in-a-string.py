3c198f7071def68a734eab8e3bd5c8ec7fd92f45
Saurabh <134841560+saur4bbh@users.noreply.github.com>
2025-09-01 14:32:07 +0200

    Time: 7 ms (8.33%), Space: 18.1 MB (23.42%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.py
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.py
@@ -1,16 +1,18 @@
 class Solution:
     def reverseWords(self, s: str) -> str:
         ans = []
-        idx = -1
+        idx = -1       
 
-        for i in range(len(s)-1,-1,-1):
-            if s[i] != ' ' and idx == -1:
-                idx = i
-            elif s[i] == ' ' and idx != -1:
+        for i in range(len(s)-1,-1,-1):       # iterating from last to first
+            
+            if s[i] != ' ' and idx == -1:     # for tracking last index of each word
+                idx = i                  
+
+            elif s[i] == ' ' and idx != -1:     # appending the word as soon as any space found
                 ans.append(s[i+1:idx+1])
                 idx = -1
 
-            if i == 0 and idx != -1:
+            if i == 0 and idx != -1:         # dealing with first word if any
                 ans.append(s[i:idx+1])
 
         return ' '.join(ans)
