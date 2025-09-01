11616a38b0a05bde41389c5aaa56d7118a09c493
Saurabh <134841560+saur4bbh@users.noreply.github.com>
2025-09-01 16:21:09 +0200

    Time: 7 ms (54.74%), Space: 18.1 MB (17.07%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.py
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -1,3 +1,13 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        return len(set(s)) == len(set(t)) == len(set(zip(s,t)))
+        d = {}
+
+        for i in range(len(s)):
+            if s[i] in d:
+                if t[i] != d[s[i]]:
+                    return False
+            elif t[i] in d.values():
+                return False
+            d[s[i]] = t[i]
+
+        return True
