79c0870d82c9e10df6b91240d92b0db760f84ada
Saurabh <134841560+saur4bbh@users.noreply.github.com>
2025-09-02 21:56:25 +0200

    Time: 6226 ms (32.7%), Space: 17.8 MB (57.16%) - LeetHub

--- 1890-sum-of-beauty-of-all-substrings/1890-sum-of-beauty-of-all-substrings.py
+++ 1890-sum-of-beauty-of-all-substrings/1890-sum-of-beauty-of-all-substrings.py
@@ -1,24 +1,20 @@
 class Solution:
     def beautySum(self, s: str) -> int:
         n = len(s)
-        if len(s)<=2:
-            return 0
-        
-        def beauty(l,r):
-            d = [0]*27
-            for i in range(l,r+1):
-                d[ord(s[i]) - ord('a')] += 1
 
-            big, small = 0, 1000
-            for i in d:
-                if i > 0:
-                    small = min(small, i)
-                    big = max(big, i)
-            return big - small
-        
         count = 0
-        for i in range(n-2):
-            for j in range(i+2,n):
-                count += beauty(i,j)
+        for i in range(n):
+            d = [0]*26
+
+            for j in range(i,n):
+                d[ord(s[j]) - ord('a')] += 1
+
+                small, big = 1000, 0
+                for i in d:
+                    if i > 0:
+                        small = min(small, i)
+                        big = max(big, i)
+
+                count += big - small
 
         return count
