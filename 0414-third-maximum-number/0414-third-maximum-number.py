883da6bd167429887a868dc6a6c7e5dfab9a354a
Saurabh <134841560+saur4bbh@users.noreply.github.com>
2025-09-04 15:26:52 +0200

    Time: 0 ms (100%), Space: 18.3 MB (97.47%) - LeetHub

--- 0414-third-maximum-number/0414-third-maximum-number.py
+++ 0414-third-maximum-number/0414-third-maximum-number.py
@@ -5,9 +5,9 @@ class Solution:
         for i in nums:
             if i > ele1:
                 ele3,ele2,ele1 = ele2,ele1,i
-            elif i > ele2 and i != ele1:
+            elif ele1 > i > ele2:
                 ele3,ele2 = ele2, i
-            elif i > ele3 and i != ele2 and i != ele1:
+            elif ele2 > i > ele3:
                 ele3 = i
                 
         return ele3 if ele3 != float('-inf') else ele1
