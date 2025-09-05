42466014920154e810672094de9c236dc6c596c9
Saurabh <134841560+saur4bbh@users.noreply.github.com>
2025-09-05 13:24:44 +0200

    Time: 7 ms (62.53%), Space: 17.7 MB (88.99%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -1,14 +1,11 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
         if x<0 : return False
+        og = x
+        rev = 0
+
+        while og != 0:
+            rev = rev*10 + og%10
+            og //= 10
         
-        l = []
-        while x != 0:
-            l.append(x%10)
-            x //= 10
-
-        for i in range(len(l)//2):
-            if l[i] != l[~i]:
-                return False
-
-        return True
+        return rev == x
