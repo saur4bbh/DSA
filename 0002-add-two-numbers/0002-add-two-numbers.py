66c89812870a54c3661cedab52596ed6dd84cb4f
Saurabh <134841560+saur4bbh@users.noreply.github.com>
2025-09-06 18:41:35 +0200

    Time: 7 ms (31.42%), Space: 18 MB (35.55%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -5,38 +5,24 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        i = l1
+        i = ListNode()
         carry = 0
+        ans = i
 
-        while l1 and l2:
-            l1.val += l2.val + carry
-            if l1.val >= 10:
-                l1.val = l1.val % 10
-                carry = 1 
-            else:
-                carry = 0
-            if not l1.next and not l2.next and carry == 1:
-                l1.next = ListNode(1,None)
-                return i
-            if not l1.next:
-                l1.next = l2.next
+        while l1 or l2 or carry == 1:
+
+            total = carry
+
+            if l1: 
+                total += l1.val
                 l1 = l1.next
-                break
-            l1 = l1.next
-            l2 = l2.next
-        
+            if l2: 
+                total += l2.val
+                l2 = l2.next
 
-        while l1:
-            l1.val += carry
-            if l1.val >= 10:
-                l1.val = l1.val % 10
-                carry = 1 
-            else:
-                carry = 0
-            if not l1.next and carry == 1:
-                l1.next = ListNode(1,None)
-                break
-            l1 = l1.next
-            
-        return i
-
+            num = total % 10
+            carry = total // 10
+            i.next = ListNode(num)
+            i = i.next
+             
+        return ans.next
