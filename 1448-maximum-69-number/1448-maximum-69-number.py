08c99bb8a308878ea0da71ecb0752db9297595d6
Saurabh <134841560+saur4bbh@users.noreply.github.com>
2025-09-06 10:09:38 +0200

    Time: 0 ms (100%), Space: 17.7 MB (77.27%) - LeetHub

--- 1448-maximum-69-number/1448-maximum-69-number.py
+++ 1448-maximum-69-number/1448-maximum-69-number.py
@@ -1,18 +1,10 @@
 class Solution:
     def maximum69Number (self, num: int) -> int:
-        nums = 0
-        while num != 0:
-            nums = nums*10 + num%10
-            num //= 10
-        
-        ans = 0 
-        rep = False
-        while nums != 0:
-            digit = nums%10
-            if not rep and digit == 6:
-                rep = True
-                digit = 9
-            ans = ans*10 + digit
-            nums //= 10
+        tup = (1,10,100,1000)
+        val = 0
+        for i in tup:
+            x = (num//i) %10
+            if x == 6:
+                val = 3 * i
 
-        return ans
+        return num + val
