7b0b1f6218c265779f2a92735e8db40e48627b0d
Saurabh <134841560+saur4bbh@users.noreply.github.com>
2025-09-08 20:18:10 +0200

    Time: 0 ms (100%), Space: 17.8 MB (42.71%) - LeetHub

---
+++ 2050-count-good-numbers/2050-count-good-numbers.py
@@ -0,0 +1,21 @@
+class Solution:
+    def countGoodNumbers(self, n: int) -> int:
+        
+        def pow(x, n, mod):
+            if n == 0:
+                return 1
+            if n%2 == 0:
+                return pow((x*x)%mod, n//2, mod)
+            else:
+                return (x * pow(x, n-1, mod))%mod
+        
+        mod = 10**9 + 7
+
+        if n % 2 == 0:
+            prime = pow(4,n//2,mod)
+            even = pow(5,n//2,mod)
+            return (prime * even) % mod
+        else:
+            prime = pow(4,n//2,mod)
+            even = pow(5,(n+1)//2,mod)
+            return (prime * even) % mod
