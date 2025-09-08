268d796a853e5aef8f36fe5a416efdf9e59db46b
Saurabh <134841560+saur4bbh@users.noreply.github.com>
2025-09-08 21:39:38 +0200

    Time: 0 ms (100%), Space: 17.7 MB (48.58%) - LeetHub

--- 2226-rings-and-rods/2226-rings-and-rods.py
+++ 2226-rings-and-rods/2226-rings-and-rods.py
@@ -1,8 +1,8 @@
 class Solution:
     def countPoints(self, rings: str) -> int:
-        rods = defaultdict(list)
+        rods = defaultdict(set)
 
         for i in range(1,len(rings),2):
-            rods[rings[i]].append(rings[i-1])
+            rods[rings[i]].add(rings[i-1])
 
-        return sum(len(set(x)) == 3 for x in rods.values())
+        return sum(len(x) == 3 for x in rods.values())
