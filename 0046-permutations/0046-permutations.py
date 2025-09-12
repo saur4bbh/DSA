2b72856e991ee9b56706c1774f0f31f2c5cadb1b
Saurabh <134841560+saur4bbh@users.noreply.github.com>
2025-09-12 19:10:49 +0200

    Time: 1 ms (47.39%), Space: 17.9 MB (82.4%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -2,24 +2,25 @@ class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         n = len(nums)
         ans = []
-        temp = []
+        part = []
 
         def backtrack():
-            if len(temp) == n:
-                ans.append(temp[:])
+            if len(part) == n:
+                ans.append(part[:])
                 return 
             
 
             for i in range(n):
                 if nums[i] != None:
-                    val = nums[i]
-                    temp.append(val)
+
+                    temp = nums[i]
+                    part.append(temp)
                     nums[i] = None
 
                     backtrack()
                     
-                    nums[i] = val
-                    temp.pop()
+                    nums[i] = temp
+                    part.pop()
 
         backtrack()
         return ans
