8a7afa4afb95da987d53ce473b36774e5a58e8cf
Saurabh <134841560+saur4bbh@users.noreply.github.com>
2025-09-13 22:32:11 +0200

    Time: 3 ms (68.56%), Space: 18 MB (45.74%) - LeetHub

---
+++ 0139-word-break/0139-word-break.py
@@ -0,0 +1,24 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        memo = {}
+        n = len(s)
+        keep = set(wordDict)
+
+        def dfs(st):
+            if st == n:
+                return True
+
+            if st in memo:
+                return memo[st]
+
+            for i in range(st, n):
+                subs = s[st : i+1]
+                
+                if subs in keep and dfs(i+1):
+                    memo[st] = True
+                    return True
+
+            memo[st] = False
+            return False
+
+        return dfs(0)
