5000fd46df7bf4feaccd8046f08c0b95cf7e32a8
Saurabh <134841560+saur4bbh@users.noreply.github.com>
2025-09-15 14:33:45 +0200

    Time: 0 ms (100%), Space: 18 MB (16.85%) - LeetHub

--- 1264-maximum-number-of-words-you-can-type/1264-maximum-number-of-words-you-can-type.py
+++ 1264-maximum-number-of-words-you-can-type/1264-maximum-number-of-words-you-can-type.py
@@ -1,10 +1,9 @@
 class Solution:
     def canBeTypedWords(self, text: str, brokenLetters: str) -> int:
         broken = set(brokenLetters)
-        
         ans = 1
-
         word = True
+
         for letter in text:
             if letter in broken and word:
                 ans -= 1
