e7bf3d091728b3cf90c23df98989f818232294ff
Saurabh <134841560+saur4bbh@users.noreply.github.com>
2025-09-16 12:41:37 +0200

    Time: 0 ms (100%), Space: 18 MB (23.04%) - LeetHub

--- 1217-relative-sort-array/1217-relative-sort-array.py
+++ 1217-relative-sort-array/1217-relative-sort-array.py
@@ -1,28 +1,22 @@
 class Solution:
     def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
-        bucket = defaultdict(int)
+        max_ele = max(arr1) + 1
+        count = [0] * max_ele
 
-        for element in arr1:
-            bucket[element] += 1
-
+        for ele in arr1:
+            count[ele] += 1
+        
         i = 0
-        for element in arr2:
-            while bucket[element]:
-                arr1[i] = element
-                bucket[element] -= 1
+        for ele in arr2:
+            while count[ele]:
+                arr1[i] = ele
+                count[ele] -= 1
                 i += 1
         
-        temp = []
-        for leftover in bucket:
-            while bucket[leftover]:
-                temp.append(leftover)
-                bucket[leftover] -= 1
-
-        j = 0
-        temp.sort()
-        for element in temp:
-            arr1[i] = temp[j]
-            i += 1
-            j += 1
+        for idx in range(max_ele):
+            while count[idx]:
+                arr1[i] = idx
+                count[idx] -= 1
+                i += 1
         
         return arr1
