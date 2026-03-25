8f02d563c2a86ab8546fffe9dd303917636d3890
Saurabh <134841560+saur4bbh@users.noreply.github.com>
2026-03-25 11:22:52 +0100

    Time: 0 ms (100%), Space: 19.3 MB (44.77%) - LeetHub

--- 0832-flipping-an-image/0832-flipping-an-image.py
+++ 0832-flipping-an-image/0832-flipping-an-image.py
@@ -5,14 +5,12 @@ class Solution:
             i = 0
             j = len(row) - 1
             while i < j:
-                row[i] = 1 - row[i]
-                row[j] = 1 - row[j]
-                row[i], row[j] = row[j], row[i]
+                row[i], row[j] = row[j] ^ 1, row[i] ^ 1
                 i += 1
                 j -= 1
 
             if i == j:
-                row[i] = 1 - row[i]
+                row[i] = row[i] ^ 1
         
         for row in image:
             reverseflip(row)
