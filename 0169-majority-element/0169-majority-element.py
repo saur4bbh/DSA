fe8244095b189731f75ad5dec6e60cf6c22272b1
Saurabh <134841560+saur4bbh@users.noreply.github.com>
2026-07-24 12:43:37 +0200

    Time: 7 ms (57.28%), Space: 21.1 MB (48.74%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -4,7 +4,11 @@ class Solution:
         ele = 0
         for i in nums:
             if cnt == 0:
-                cnt,ele = 1,i
-            elif ele == i: cnt += 1
-            else: cnt -= 1
+                ele = i
+                cnt += 1
+            elif i == ele:
+                cnt += 1
+            else:
+                cnt -= 1
+        
         return ele
