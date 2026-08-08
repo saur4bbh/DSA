074fdf914b9e57d96ce5d835a82e5b2a77091731
Saurabh <134841560+saur4bbh@users.noreply.github.com>
2026-08-08 18:39:04 +0200

    Time: 0 ms (100%), Space: 19.1 MB (86.21%) - LeetHub

--- 3925-concatenate-array-with-reverse/3925-concatenate-array-with-reverse.py
+++ 3925-concatenate-array-with-reverse/3925-concatenate-array-with-reverse.py
@@ -1,10 +1,6 @@
 class Solution:
     def concatWithReverse(self, nums: list[int]) -> list[int]:
-        ans = []
-        for i in range(len(nums)*2):
-            if i >= len(nums):
-                ans.append(nums[len(nums)*2 - i - 1])
-            else:
-                ans.append(nums[i])
-
+        ans = nums.copy()
+        for i in range(len(nums) - 1, -1, -1):
+            ans.append(nums[i])
         return ans
